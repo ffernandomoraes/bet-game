cd4b2d5e519ba7eac869fa59ffe0ead800d7b2cb
Fernando Moraes <fernando.moraees@eduzz.com>
2020-09-03 14:35:03 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bet-game/precache-manifest.14323e8d75870eb84e17d9f465987fec.js"
+  "https://ffernandomoraes.github.io/bet-game/precache-manifest.9399e967669d148c0f99cffe3bb14dd3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bet-game/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://ffernandomoraes.github.io/bet-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
